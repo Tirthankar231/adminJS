6f3ccdbfeb4d7c93b7a57af868c818c153073d35
Tirthankar231 <142375115+Tirthankar231@users.noreply.github.com>
2024-04-17 20:55:01 +0530

    Delete database.js

--- database.js
+++
@@ -1,6 +0,0 @@
-// File: database.js
-import { Sequelize } from 'sequelize';
-
-const sequelize = new Sequelize('postgresql://postgres:Tirtha@4321@localhost:5432/adminJS');
-
-export default sequelize;
