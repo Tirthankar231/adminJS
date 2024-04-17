bcfa8f0184d1cfc770a893542091520ce13016e7
Tirthankar231 <142375115+Tirthankar231@users.noreply.github.com>
2024-04-17 20:55:42 +0530

    Add files via upload

---
+++ database.js
@@ -0,0 +1,8 @@
+// File: database.js
+import { Sequelize } from 'sequelize';
+
+const sequelize = new Sequelize('postgresql://postgres:Tirtha@4321@localhost:5432/adminJS', {
+    dialect: 'postgres',
+  });
+
+export default sequelize;
---
+++ models/User.js
@@ -0,0 +1,38 @@
+import { DataTypes, Model } from 'sequelize';
+import sequelize from '../database.js';
+
+class User extends Model {}
+
+User.init(
+  {
+    id: {
+      type: DataTypes.UUID,
+      primaryKey: true,
+      defaultValue: DataTypes.UUIDV4,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    createdAt: {
+      type: DataTypes.DATE,
+      allowNull: true,
+    },
+    updatedAt: {
+      type: DataTypes.DATE,
+      allowNull: true,
+    },
+  },
+  {
+    sequelize,
+    tableName: 'users',
+    modelName: 'User'
+  }
+);
+
+export { User }; 
